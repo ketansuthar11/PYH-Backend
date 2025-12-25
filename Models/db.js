f741711958b2039f7bc642620ec358e25c370478
Ketan Suthar <159815056+ketansuthar11@users.noreply.github.com>
2025-12-25 15:02:31 +0530

    Update db.js

--- Models/db.js
+++ Models/db.js
@@ -1,8 +1,22 @@
-const mongoose = require('mongoose');
-const mongo_url = process.env.MONGO_CONN;
+const mongoose = require("mongoose");
 
-mongoose.connect(mongo_url).then(()=>{
-    console.log("Sucessfully connected");
-}).catch((err)=>{
-    console.log("Connction failed",err);
-});
+// agar local pe .env use karte ho to ye rehne do
+require("dotenv").config();
+
+const connectDB = async () => {
+  try {
+    console.log("MONGO_CONN =>", process.env.MONGO_CONN); // debug ke liye
+
+    await mongoose.connect(process.env.MONGO_CONN, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection failed:", err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
